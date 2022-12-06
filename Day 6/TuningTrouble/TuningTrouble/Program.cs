6c3c050c42cc777ee98bd7a29235ebc6839f85b8
Phillip Stewart <peastew@gmail.com>
2022-12-05 23:14:15 -0600

    Day 6 advent of code. I sleep.

---
+++ Day 6/TuningTrouble/TuningTrouble/Program.cs
@@ -0,0 +1,56 @@
+﻿
+PartOne();
+
+PartTwo();
+
+void PartOne()
+{
+    using var input = new StreamReader("input.txt");
+
+    int totalCount = 0;
+    Queue<char> buffer = new Queue<char>();
+
+    while (!input.EndOfStream)
+    {
+        var nextChar = (char)input.Read();
+        totalCount++;
+
+        buffer.Enqueue(nextChar);
+
+        if (buffer.Count > 4)
+            buffer.Dequeue();
+
+        if(new HashSet<char>(buffer).Count == 4)
+        {
+            break;
+        }
+    }
+
+    Console.WriteLine(totalCount);
+}
+
+void PartTwo()
+{
+    using var input = new StreamReader("input.txt");
+
+    int totalCount = 0;
+    Queue<char> buffer = new Queue<char>();
+
+    while (!input.EndOfStream)
+    {
+        var nextChar = (char)input.Read();
+        totalCount++;
+
+        buffer.Enqueue(nextChar);
+
+        if (buffer.Count > 14)
+            buffer.Dequeue();
+
+        if (new HashSet<char>(buffer).Count == 14)
+        {
+            break;
+        }
+    }
+
+    Console.WriteLine(totalCount);
+}
