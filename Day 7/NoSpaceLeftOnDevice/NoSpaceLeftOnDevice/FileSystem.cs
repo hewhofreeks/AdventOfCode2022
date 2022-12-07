f47fbd8e17d5f79189fceff292f18b3dbd6f7d3b
Phillip Stewart <peastew@gmail.com>
2022-12-07 11:28:51 -0600

    Day 7 solution. Breadth first search these files!

---
+++ Day 7/NoSpaceLeftOnDevice/NoSpaceLeftOnDevice/FileSystem.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoSpaceLeftOnDevice
+{
+    class BaseFile
+    {
+        public BaseFile(string name)
+        {
+            Name = name;
+        }
+        public BaseFile(string name, int size)
+        {
+            Name = name;
+            Size = size;
+        }
+
+        public string Name { get; private set; }
+
+        public virtual int Size { get; }
+    }
+
+    class Directory : BaseFile
+    {
+        public Directory(string name, Directory parent) : base(name)
+        {
+            Files = new List<BaseFile>();
+            Parent = parent;
+        }
+
+        public Directory(string name) : base(name)
+        {
+            Files = new List<BaseFile>();
+        }
+
+        public Directory Parent { get; set; }
+
+        public IList<BaseFile> Files { get; }
+
+        public override int Size
+        {
+            get
+            {
+                return Files.Sum(f => f.Size);
+            }
+        }
+
+        public void AddFile(BaseFile file)
+        {
+            Files.Add(file);
+        }
+    }
+
+    class FileSystem
+    {
+        private Directory _rootDirectory;
+        private Directory _currentDirectory;
+
+        public FileSystem()
+        {
+            _rootDirectory = new Directory("/");
+            _currentDirectory = _rootDirectory;
+        }
+
+        public Directory GetRoot()
+        {
+            return _rootDirectory;
+        }
+
+        public void ChangeDirectory(string input)
+        {
+            switch (input)
+            {
+                case "..":
+                    if (_currentDirectory.Parent != null)
+                        _currentDirectory = _currentDirectory.Parent;
+                    break;
+                case "/":
+                    _currentDirectory = _rootDirectory;
+                    break;
+                default:
+                    var directoryToNavigateTo = _currentDirectory.Files.OfType<Directory>().FirstOrDefault(f => f.Name == input);
+
+                    if(directoryToNavigateTo != null)
+                        _currentDirectory = directoryToNavigateTo;
+                    
+                    break;
+            }
+        }
+
+        public void AddFileToCurrentDirectory(string name, int size)
+        {
+            if (_currentDirectory.Files.Any(f => f.Name == name))
+                return;
+
+            _currentDirectory.AddFile(new BaseFile(name, size));
+        }
+
+        public void AddDirectoryToCurrentDirectory(string name)
+        {
+            if (_currentDirectory.Files.Any(f => f.Name == name))
+                return;
+
+            _currentDirectory.AddFile(new Directory(name, _currentDirectory));
+        }
+    }
+}
---
+++ Day 7/NoSpaceLeftOnDevice/NoSpaceLeftOnDevice/Program.cs
@@ -0,0 +1,103 @@
+﻿
+using NoSpaceLeftOnDevice;
+
+
+using var input = new StreamReader("input.txt");
+
+// Ignore first line since we're gonna start in the root
+input.ReadLine();
+var fileSystem = new FileSystem();
+
+// Generate file system
+while (!input.EndOfStream)
+{
+    var arg = input.ReadLine().Split(' ');
+
+    if (arg[0] == "$" && arg[1] == "cd")
+    {
+        fileSystem.ChangeDirectory(arg[2]);
+    }
+    else if (arg[0] != "$")
+    {
+        // If it's not a command, we're just listing files. try to add them to the current directory
+
+        if (arg[0] == "dir")
+        {
+            fileSystem.AddDirectoryToCurrentDirectory(arg[1]);
+        }
+        else
+        {
+            fileSystem.AddFileToCurrentDirectory(arg[1], int.Parse(arg[0]));
+        }
+    }
+}
+
+PartOne();
+
+PartTwo();
+
+void PartOne()
+{
+    var directories = FindAllDirectoriesWithSize(fileSystem, 100000);
+
+    var sizeOf = directories.Sum(d => d.Size);
+
+    Console.WriteLine(sizeOf);
+}
+
+void PartTwo()
+{
+    var dir = FindSmallestDirectoryToFreeUp(fileSystem, 70000000, 30000000);
+
+    Console.WriteLine(dir.Size);
+}
+
+IEnumerable<NoSpaceLeftOnDevice.Directory> FindAllDirectoriesWithSize(FileSystem fileSystem, int maxSize)
+{
+    Queue<NoSpaceLeftOnDevice.Directory> bfsQueue = new Queue<NoSpaceLeftOnDevice.Directory>();
+    List<NoSpaceLeftOnDevice.Directory> results = new List<NoSpaceLeftOnDevice.Directory>();
+
+    bfsQueue.Enqueue(fileSystem.GetRoot());
+
+    while (bfsQueue.TryDequeue(out var dir))
+    {
+        if (dir.Size <= maxSize)
+        {
+            results.Add(dir);
+        }
+
+        foreach (var directory in dir.Files.OfType<NoSpaceLeftOnDevice.Directory>())
+            bfsQueue.Enqueue(directory);
+
+    }
+
+    return results;
+}
+
+NoSpaceLeftOnDevice.Directory FindSmallestDirectoryToFreeUp(FileSystem fileSystem, int totalSize, int neededSpace)
+{
+    Queue<NoSpaceLeftOnDevice.Directory> bfsQueue = new Queue<NoSpaceLeftOnDevice.Directory>();
+    
+    var root = fileSystem.GetRoot();
+    NoSpaceLeftOnDevice.Directory result = root;
+
+    var currentFreeSpace = totalSize - root.Size;
+    var spaceNeededToMakeAvailable = neededSpace - currentFreeSpace;
+
+
+    bfsQueue.Enqueue(root);
+
+    while (bfsQueue.TryDequeue(out var dir))
+    {
+        if (dir.Size >= spaceNeededToMakeAvailable)
+        {
+            if (dir.Size < result.Size)
+                result = dir;
+
+            foreach (var directory in dir.Files.OfType<NoSpaceLeftOnDevice.Directory>())
+                bfsQueue.Enqueue(directory);
+        }
+    }
+
+    return result;
+}
