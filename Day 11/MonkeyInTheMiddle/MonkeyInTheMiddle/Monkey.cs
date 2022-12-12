3cdf1582f7f36eef262d044b9a2ca546cf7fe56e
Phillip Stewart <peastew@gmail.com>
2022-12-11 19:57:39 -0600

    Solution for day 11. Big numbers bad. Needed help for this one.

---
+++ Day 11/MonkeyInTheMiddle/MonkeyInTheMiddle/Monkey.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyInTheMiddle
+{
+    public class Monkey
+    {
+        public Queue<ulong> Items { get; set; } = new Queue<ulong>();
+
+        public uint InspectionCount { get; set; } = 0;
+
+        public Func<Monkey, ulong> LeftSideOfOperation { get; set; }
+
+        public Func<Monkey, ulong> RightSideOfOperation { get; set; }
+
+        public string Operation { get; set; }
+
+        public ulong TestNum { get; set; }
+
+        public int ThrowToMonkeyIfFalse { get; set; }
+        public int ThrowToMonkeyIfTrue { get; set; }
+
+
+        public (bool worryTrue, ulong newWorryValue) TestWorry(ulong MOD)
+        {
+            InspectionCount++;
+
+            var left = LeftSideOfOperation(this);
+            var right = RightSideOfOperation(this);
+            ulong total = 0;
+
+            if(Operation == "+")
+            {
+                total = left + right;
+            }
+            else
+            {
+                total = left * right;
+            }
+
+            total %= MOD;
+
+           //total = total / 3;
+
+            return (total % TestNum == 0, total);
+        }
+
+    }
+}
---
+++ Day 11/MonkeyInTheMiddle/MonkeyInTheMiddle/Program.cs
@@ -0,0 +1,81 @@
+﻿using MonkeyInTheMiddle;
+using System.Text.RegularExpressions;
+
+using var input = new StreamReader("input.txt");
+
+List<Monkey> monkeys = new List<Monkey>();
+ulong SUPER_MOD = 1;
+
+while(!input.EndOfStream)
+{
+    var monkey = new Monkey();
+    var number = input.ReadLine(); // Monkey 0:
+    var startingItems = input.ReadLine(); //  Starting items: 79, 98
+    var items = Regex.Matches(startingItems, @"\d+").Select(s => ulong.Parse(s.Value));
+    foreach(var item in items)
+        monkey.Items.Enqueue(item);
+
+    var operation = input.ReadLine(); //  Operation: new = old * 19
+    var opValue = Regex.Match(operation, @"= (.*)").Value.Split(" ");
+    if (opValue[1] == "old")
+    {
+        monkey.LeftSideOfOperation = (m) => m.Items.Peek();
+    }
+    else
+    {
+        monkey.LeftSideOfOperation = (m) => ulong.Parse(opValue[1]);
+    }
+
+    monkey.Operation = opValue[2];
+
+    if (opValue[3] == "old")
+    {
+        monkey.RightSideOfOperation = (m) => m.Items.Peek();
+    }
+    else
+    {
+        monkey.RightSideOfOperation = (m) => ulong.Parse(opValue[3]);
+    }
+
+    var test = input.ReadLine(); //   Test: divisible by 23
+    monkey.TestNum = ulong.Parse(Regex.Match(test, @"\d+").Value);
+
+    SUPER_MOD *= monkey.TestNum;
+
+    var ifTrue = input.ReadLine(); //     If true: throw to monkey 2
+    monkey.ThrowToMonkeyIfTrue = int.Parse(Regex.Match(ifTrue, @"\d+").Value);
+
+    var ifFalse = input.ReadLine(); //    If false: throw to monkey 3
+    monkey.ThrowToMonkeyIfFalse = int.Parse(Regex.Match(ifFalse, @"\d+").Value);
+
+    monkeys.Add(monkey);
+    if(!input.EndOfStream)
+    input.ReadLine();
+}
+
+Console.WriteLine("Done creating monkeys");
+
+for(int i = 0;i<10000;i++)
+{
+    foreach(var monkey in monkeys)
+    {
+        while(monkey.Items.TryPeek(out var item))
+        {
+            var (worryResults, newVal) = monkey.TestWorry(SUPER_MOD);
+            var toThrow = monkey.Items.Dequeue();
+
+            if (worryResults)
+            {
+                monkeys[monkey.ThrowToMonkeyIfTrue].Items.Enqueue(newVal);
+            }
+            else
+            {
+                monkeys[monkey.ThrowToMonkeyIfFalse].Items.Enqueue(newVal);
+            }
+        }
+    }
+}
+
+var monkeyBusiness = monkeys.Select(m => m.InspectionCount).OrderDescending().Take(2).ToArray();
+
+Console.WriteLine($"{monkeyBusiness[0]} * {monkeyBusiness[1]}");
